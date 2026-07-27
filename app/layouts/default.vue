c119f4e818dc032acd602d61167258ae725d9e36
NayakSanghamitra <139398622+NayakSanghamitra@users.noreply.github.com>
2026-07-27 13:46:30 +0530

    Update default.vue

--- app/layouts/default.vue
+++ app/layouts/default.vue
@@ -1,6 +1,6 @@
 <template>
   <v-app class="bg-grey-lighten-4">
-    <!-- 'app' prop fixes the overlapping header issue -->
+    <!-- 'app' prop reserves space so headers NEVER hide content underneath -->
     <v-app-bar app elevation="1" color="surface">
       <v-container fluid class="d-flex align-center py-0 px-6">
         <v-icon icon="mdi-chart-timeline-variant" color="primary" size="28" class="mr-3" />
@@ -32,9 +32,9 @@
       </v-container>
     </v-app-bar>
 
-    <!-- 'app' prop ensures v-main starts BELOW the header -->
+    <!-- 'app' prop on v-main moves content below the navigation bar -->
     <v-main app>
-      <!-- 'fluid' spans the container edge-to-edge without excess side margins -->
+      <!-- 'fluid' fills left-to-right without large empty side margins -->
       <v-container fluid class="pa-6">
         <slot />
       </v-container>
