ba9c6d40788fb3d435b006821b1a1a1f97a32016
NayakSanghamitra <139398622+NayakSanghamitra@users.noreply.github.com>
2026-07-27 15:58:10 +0530

    Create chartjs.client.ts

---
+++ app/plugins/chartjs.client.ts
@@ -0,0 +1,26 @@
+import {
+  Chart as ChartJS,
+  Title,
+  Tooltip,
+  Legend,
+  BarElement,
+  CategoryScale,
+  LinearScale,
+  ArcElement,
+  DoughnutController,
+  BarController
+} from 'chart.js'
+
+export default defineNuxtPlugin(() => {
+  ChartJS.register(
+    Title,
+    Tooltip,
+    Legend,
+    BarElement,
+    CategoryScale,
+    LinearScale,
+    ArcElement,
+    DoughnutController,
+    BarController
+  )
+})
