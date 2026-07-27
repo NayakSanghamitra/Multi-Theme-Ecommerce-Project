52db09c3415641fbd2adadd1b632df19fc09a988
NayakSanghamitra <139398622+NayakSanghamitra@users.noreply.github.com>
2026-07-27 17:48:39 +0530

    Update default.vue

--- app/layouts/default.vue
+++ app/layouts/default.vue
@@ -1,7 +1,7 @@
 <template>
   <v-app class="bg-grey-lighten-4">
     <!-- `app` + `position="fixed"` forces the header to lock to the top of the viewport -->
-    <v-app-bar app position="fixed" elevation="1" color="surface" height="64">
+    <v-app-bar app class="position-fixed" elevation="1" color="surface" height="64">
       <v-container fluid class="d-flex align-center py-0 px-6">
         <v-icon icon="mdi-chart-timeline-variant" color="primary" size="28" class="mr-3" />
         <v-app-bar-title class="font-weight-bold text-h6 text-primary">
